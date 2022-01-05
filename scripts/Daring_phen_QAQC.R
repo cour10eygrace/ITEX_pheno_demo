98422e0a97dcdf3ee550b12bcb02d15caaa69a37
cour10eygrace <courtneygrace125@gmail.com>
2022-01-04 17:42:47 -0800

    add prior visits to phenology observations and clean up/save full dataset

--- scripts/Daring_phen_QAQC.R
+++ scripts/Daring_phen_QAQC.R
@@ -5,36 +5,13 @@ library (tidyr)
 library (magrittr)
 library (readxl)
 
-
-setwd("C:/Users/court/Google Drive/UBC Postdoc/Daring_phenology")
-
 #from Sarah's code 
 # source useful functions, findNonnumeric here
-source('utility_functions_all.R')
-
-prior_visit=function(x){
-  if(grepl('^<', x)){
-    NA
-  }else if (grepl('^>', x)){
-    gsub('^>', '', x)
-  }else if (grepl('<|>|-', x)){
-    gsub('>.*|-.*|<.*', '', x)
-  }else{NA}
-}
-
-phen_visit=function(x){
-  if(grepl('^<', x)){
-    gsub('^<', '', x)
-  }else if (grepl('^>', x)){
-    NA
-  }else if (grepl('<|>|-', x)){
-    gsub('.*>|.*-|.*<', '', x)
-  }else{x}
-}
+source('scripts/utility_functions_all.R')
 
 
 # -- READ PHENOLOGY DATA ----
-excelfile='2021 ITEX multi-year phenology.xlsx'
+excelfile='data/2021 ITEX multi-year phenology_clean.xlsx'
 sheets=excel_sheets(excelfile)
 
 new_data=list()
@@ -75,26 +52,31 @@ new_data=lapply(new_data, function(x) setNames(x, gsub('NA_|_NA', '', names(x)))
 cols_to_keep=c(#all
                "Location", "Year", "ID no.", "sheet", "Notes", 
                #oxtytropis
-               "P1( MET) snow free","P1 (obs)a snow free",
+              #"P1( MET) snow free", #leave these out for now and just use on the ground obs
+               "P1 (obs)a snow free",
                "P2a 1st green leaf","P2b 1st flower bud","P3 1st flower open","P4 1st petal drop",
                "P5 last petal drop","P6 1st seed shed","Q1a no. of buds", "Q1b no. of pods", "mean diametre", 
                #ledum
-               "P1 (MET) snow free", "P1 (obs) snow free", 
+               #"P1 (MET) snow free",
+               "P1 (obs) snow free", 
                "P2 flower buds visible","P3 first flower open", 
                "P4 first flower shed",  "P5 last flower shed","P6 first fruit visible",  
                "Q1 no. of flowering stalks", "Q2 number of flowers/ stalk", "Q3 number of fruit / stalk", 
                "mean growth increment(mm)",
                #vaccinium
+               "P1(obs) snow free",
                "P2 flower bud visible", "Q1 no. of flowers", "Q2 no. of fruit", 
                #betula
-               "P1(MET)", "P1(obs)",
+               #"P1(MET)",
+               "P1(obs)",
                "P2_1st leaf",  "P3a_1st male catkin","P3b_1st female catkin",
-               "P4a_1st stigma","P4b_pollen shed",   "P5a_Ist leaf turn",
+               "P4a_1st stigma","P4b_pollen shed",   "P5a_1st leaf turn",
                "P5b_last leaf turn","P6_1st leaf shed",  "P7_all shed",  
-               "Q1_no. male catkins", "Q2_no. female", "total_catkin", "Mean leaf  length (mm)", 
+               "Q1_no. male catkins", "Q2_no. female", "Mean leaf  length (mm)", 
                "Mean growth increment (mm)",
                #salix 
-               "P1(MET)_snow",  "P1(obs)_snow free",
+               #"P1(MET)_snow", 
+               "P1(obs)_snow free",
                "P2_1st leaf bud",  
                "P3a_1st stigma","P3b_pollen shed",   "P4_1st leaf turn", 
                "P5_last leaf turn", "P6_1st abs. of leaf","P7_all leaf shed", 
@@ -109,10 +91,6 @@ cols_to_keep=c(#all
                #carex
                "P3_1st stigma", "P4_1st anther", "P5_yellow leaf",   
                "P6_seed shed","Sum green_leaf length")
-
-#names=lapply(new_data, function(x) names(x))%>%unlist()%>%unique()
-#grep('^P', names, value=TRUE)
-#lapply(new_data, function(x) names(x))
 
 #select only the cols you want
 phen=lapply(new_data, function(x) x=x[,names(x)%in%cols_to_keep])
@@ -158,33 +136,37 @@ dup2=which(phen$`ID no.`=='319'&phen$Year==2012&!is.na(phen$Year)&phen$species==
 phen=phen[-dup2,]
 
 #make long
-#filter out non-phen 
+#separate phen and non-phen 
 phen%<>%filter(!is.na(`ID no.`))%>%
-  gather(key='phen_stage', value='doy', -c('species','treatment', 'Location','Year', 
-                                           'ID no.', 'Sex', 'Notes',"Q1a no. of buds",	"Q1b no. of pods",	"mean diametre", 
-                                           "mean no. flowers/ stalk", "mean no. fruit/ stalk", "mean growth increment(mm)", 
-                                           "Q1 no. of flowers",	"Q2 no. of fruit",	"mean growth increment(mm)" ,
-                                           "Q1_no. male catkins", "Q2_no. female", "total_catkin", "Mean leaf  length (mm)", 
+ gather(key='phen_stage', value='doy', -c('species','treatment', 'Location','Year', 
+                                           'ID no.', 'Notes',"Q1a no. of buds",	"Q1b no. of pods",	"mean diametre", 
+                                            "mean growth increment(mm)", "Q1_no. stalks", "Q1 no. of flowering stalks",
+                                            "Q1 no. of flowers",	"Q2 no. of fruit",	"mean growth increment(mm)" ,
+                                           "Q1_no. male catkins", "Q2_no. female", "Mean leaf  length (mm)", 
                                            "Mean growth increment (mm)",
                                            "Q1_total no. catkins", "Q2_longest leaf (mm)", "mean_growth increment",
-                                           "mean diameter (mm)",  
-                                           "mean shaft_length", "Mean_shaft length", "Q3_mean", "Q4", "mean_leaf length", "Mean_diametre",
+                                           "mean diameter (mm)",  "Q2 number of flowers/ stalk", 
+                                           "Q3 number of fruit / stalk",
+                                          "Q3_mean",  "mean_leaf length", "Mean_diametre",
                                            "Q3_mean", "Sum green_leaf length"))
 
-phen_trait<-filter(phen, !is.na(`ID no.`))%>%
+phen<-filter(phen, !is.na(`ID no.`))%>%
   gather(key='trait', value='value', -c('species','treatment', 'Location','Year', 
-                                           'ID no.', 'Sex', 'Notes','phen_stage', 'doy'))
+                                           'ID no.', 'Notes','phen_stage', 'doy'))
 
-#remove phenophases for which there is NEVER data
+#check both
+sort(unique(phen$phen_stage))
+sort(unique(phen$trait))
+
+#remove phenophases for which there is NEVER data-mostly due to diff naming across spp 
 # these not relevant in keeping NAs
 allmissing=phen%>%group_by(species, phen_stage)%>%
   summarize(has_data=sum(!is.na(doy)))%>%
   mutate(has_any_data=ifelse(has_data>0, TRUE, FALSE))%>%
   filter(has_any_data>0)
-
+#keeps only values with data 
 phen%<>%
   inner_join(., allmissing%>%select(species, phen_stage))
-
 
 #clean up one typo, <142 in 2000 should be >2000
 #sf doy 151 misentered as 15, etc, can tell due to synchrony and days visited
@@ -210,40 +192,130 @@ phen$doy[phen$doy=='144<144'&!is.na(phen$doy)&phen$Year=='2013']='144<146'
 phen$doy[phen$doy=='192-192'&!is.na(phen$doy)&phen$Year=='2010']='190-192'
 phen$doy[phen$doy=='260'&!is.na(phen$doy)&phen$Year=='2007']='206'
 
-
-
-for (k in 1:nrow(phen)){
-  phen$prior_visit[k]=prior_visit(phen$doy[k])
-  phen$phen_visit[k]=phen_visit(phen$doy[k])
-}
 #put these in comments, then set these to NA
-phen$Notes[phen$phen_visit%in%c('0', 's', "206\\s", "x",
-                                "Female", "grazed", "eaten", "209s")&
-             !is.na(phen$phen_visit)]=
-  paste(phen$Notes[phen$phen_visit%in%c('0', 's', "206\\s", "x",
-                                        "Female", "grazed", "eaten", "209s")&
-                     !is.na(phen$phen_visit)],phen$phen_visit[phen$phen_visit%in%c('0', 's', "206\\s", "x",
-                                                                                   "Female", "grazed", "eaten", "209s")&
-                                                                !is.na(phen$phen_visit)], sep=';')
+phen$Notes[phen$doy%in%c('0', 's', "206\\s", "x",
+                         "Female", "grazed", "eaten", "209s")&
+             !is.na(phen$doy)]=
+  paste(phen$Notes[phen$doy%in%c('0', 's', "206\\s", "x",
+                                 "Female", "grazed", "eaten", "209s")&
+                     !is.na(phen$doy)],phen$doy[phen$doy%in%c('0', 's', "206\\s", "x",
+                                                              "Female", "grazed", "eaten", "209s")&
+                                                  !is.na(phen$doy)], sep=';')
 
-phen$phen_visit[phen$phen_visit%in%c('0', 's', "206\\s", "209s", "x",
-                                     "Female", "grazed", "eaten")&
-                  !is.na(phen$phen_visit)]=NA
+phen$doy[phen$doy%in%c('0', 's', "206\\s", "209s", "x",
+                       "Female", "grazed", "eaten")&
+           !is.na(phen$doy)]=NA
 
 
-#remove question makrs, retain the 191
-phen$phen_visit[phen$phen_visit=="or=191"&
-                  !is.na(phen$phen_visit)]='191'
+#remove question marks, retain the 191
+phen$doy[phen$doy=="or=191"&
+                  !is.na(phen$doy)]='191'
 
-phen$phen_visit[grepl('\\?', phen$phen_visit)&
-                  !is.na(phen$phen_visit)]=NA
+phen$doy[grepl('\\?', phen$doy)&
+                  !is.na(phen$doy)]=NA
 
-#convert all to numeric
-phen%<>%mutate(phen_visit=as.numeric(phen_visit),
-               prior_visit=as.numeric(prior_visit))
+
+#update naming on sf carex
+phen<-mutate(phen,phen_stage= case_when(phen_stage=='obs'~'obs snow free', 
+                            # phen_stage=='MET'~'MET snow free', 
+                             TRUE~phen_stage))
+
+
+#remove traits for which there is NEVER data-mostly due to diff naming across spp 
+# these not relevant in keeping NAs
+allmissing2=phen%>%group_by(species, trait)%>%
+  summarize(has_data=sum(!is.na(value)))%>%
+  mutate(has_any_data=ifelse(has_data>0, TRUE, FALSE))%>%
+  filter(has_any_data>0)
+#keeps only values with data 
+phen%<>%
+  inner_join(., allmissing2%>%select(species, trait))
+
+#deal with ranges in doys 
+#separate ranges into prior visit and phen visit 
+phen2<-filter(phen, grepl("<|-|>", doy))%>%
+  separate(doy, 
+           into = c("prior_visit", "phen_visit"), 
+           sep = "<|-|>", remove=F)
+
+phen<-filter(phen, !grepl("<|-|>", doy)) #take out the ones with ranges 
+phen$phen_visit<-phen$doy
+phen$prior_visit<-NA
+
+phen<-select(phen, "Location", "Year", "ID no.","Notes", "species","treatment", "phen_stage","doy",
+             "prior_visit", "phen_visit", "trait", "value")
+
+phen<-rbind(phen, phen2)#put back in separated 
+
+#check 
+check<-select(phen, doy, prior_visit, phen_visit)%>%distinct(.)
+
+#update where >doy should be prior visit 
+phen<-mutate(phen, prior_visit2=case_when(doy==">168"~"168", 
+                                           doy==">185"~"185", 
+                                           doy==">211"~"211",
+                                           doy==">212"~"212", 
+                                           doy==">214"~"214",  
+                                           doy==">222"~"222"))%>%
+  mutate(prior_visit=if_else(!is.na(prior_visit2), prior_visit2, prior_visit))%>%
+  mutate(phen_visit=ifelse(!is.na(prior_visit2), NA,phen_visit))%>%select(-prior_visit2)%>%
+  mutate(phen_visit=case_when(phen_visit=="or=191"~"191", 
+                               TRUE~phen_visit)) #get rid of less than or equal to 
+#check again
+check2<-select(phen, doy, prior_visit, phen_visit)%>%distinct(.)
 
 #keep one original copy of the prior_visit and doy not imputed
 phen%<>%mutate(prior_visit_orig=prior_visit)%>%
   rename(doy_orig=doy)%>%
   rename(doy=phen_visit)%>%
   rename(year=Year, plant_id="ID no.")
+#convert all to numeric
+phen<-phen%<>%mutate(doy=as.numeric(doy),
+                     prior_visit=as.numeric(prior_visit))
+
+#remove rows with neither doy nor prior visit 
+phen_nas<-subset(phen, is.na(prior_visit)&is.na(doy))
+phen<-anti_join(phen, phen_nas)
+
+# -- ASSIGN PRIOR VISITS ----
+#pull out all observation dates for each year x spp
+obsdates <- phen %>% select(year, species, doy) %>% distinct()
+# reassign names
+names(obsdates) <- c('year', 'species', "Obs.date")
+
+#find observation date with the minimum difference to the doy of the phenstage for the same spp x year
+#often the obs snow free
+phenxx<-left_join(phen, obsdates)%>%group_by(year, species, treatment)%>%filter(!is.na(doy))%>%
+  mutate(diff=doy-Obs.date)%>%group_by(year, species, treatment,doy, phen_stage)%>%filter(diff>0)%>%
+    slice(which.min(diff))%>%
+  select(year, species, treatment, phen_stage, Obs.date)
+
+#join with full dataset
+#if prior visit is NA fill in with min obs date
+phen<-left_join(phen, phenxx)%>%
+  mutate(prior_visit=if_else(is.na(prior_visit), Obs.date, prior_visit))%>%
+  select(-Obs.date)
+#check all phenology where pv not assigned
+pvmiss<-subset(phen, is.na(prior_visit)& phen_stage!="obs snow free")%>%
+  select( species, year, phen_stage)%>%distinct(.)
+#either the case that snow free was not observed in that year/spp 
+#or that snow free entered as coarse date range which is same as first phenophases so diff=0
+#either way not informative for prior visits of early season phenology so remove these observations 
+
+phen
+#Finally filter for phenology only (remove snow free) WITH prior visits
+#remove observations with no demography (traits) and without 
+phen_dem<-filter(phen, phen_stage!="obs snow free")%>% 
+  filter(!is.na(prior_visit))%>%
+  filter(!is.na(value))
+
+
+#remove anything where the prior visit window is >14 days 
+#and then take average bw prior visit and doy as censored phen observation
+phen_dem<-mutate(phen_dem, diff=doy-prior_visit)%>%filter(diff<15)%>%
+  select(-diff)%>%
+  mutate(DOY=(prior_visit+doy)/2)
+plot(phen_dem$DOY~phen_dem$doy)
+
+save(phen_dem, file='data/DLphen_w_priorvisit.Rdata')
+#load('data/DLphen_w_priorvisit.Rdata')
