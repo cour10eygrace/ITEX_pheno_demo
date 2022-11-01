cb637f36bdd9e98a05965ce0f982294c4be8f249
cour10eygrace <courtneygrace125@gmail.com>
2022-11-01 15:39:12 -0700

    add custom color pallette

--- scripts/Alex_plotcover_QAQC.R
+++ scripts/Alex_plotcover_QAQC.R
@@ -57,6 +57,15 @@ xx$otc_treatment <- factor(xx$otc_treatment, levels=order)
     #geom_point()+
     facet_wrap(~species, scales="free")+theme_classic()+
     theme(axis.text.x = element_text(angle = 90))
+
+specColor <- c(
+  "#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7",
+  "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD",
+  "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D",
+  "#8A7C64", "#599861", "#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7",
+  "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD",
+  "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D",
+  "#8A7C64", "#599861")
   
 
   
