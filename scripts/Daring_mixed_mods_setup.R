5c7aad65e7ee24a5e38013d3cb637dd37f0d8868
cour10eygrace <courtneygrace125@gmail.com>
2023-03-02 12:01:13 -0800

    remove GDDs, flower buds

--- scripts/Daring_mixed_mods_setup.R
+++ scripts/Daring_mixed_mods_setup.R
@@ -13,30 +13,14 @@ daring_phen_lag<-select(daring_phen_long, -phen, -doy, -av_air_temp_C, -sfDOY)%>
   mutate(lagtime=year-year_lag)%>%
   mutate(value_lag=if_else(lagtime>1, NA_real_, value_lag))
 
-#GDD lags 
-#determine prev years' GDD for enviro lag effects 
-names(daring_phen_long)
-GDD_lag<-select(daring_phen_long, species, plantid, treatment, phen, year, GDD)%>%distinct(.)%>%
-  group_by(species, plantid, treatment, phen)%>%arrange(year)%>%
-  mutate(year=as.numeric(year), GDD_lag=lag(GDD), year_lag=lag(year))%>%
-  mutate(lagtime=year-year_lag)%>%
-  mutate(GDD_lag=if_else(lagtime>1, NA_real_, GDD_lag))%>%select(-year_lag, -lagtime)%>%
-  distinct(.)
-
-
 #merge back with phen data 
 daring_phen_lag$year<-as.character(as.integer(daring_phen_lag$year))
 daring_phen_long<-left_join(daring_phen_long, select(daring_phen_lag,  -year_lag, -lagtime))
 
-GDD_lag$year<-as.character(as.integer(GDD_lag$year))
-daring_phen_long<-left_join(daring_phen_long, GDD_lag)
-
 #raw values
-plot(daring_phen_long$GDD~daring_phen_long$GDD_lag)
-cor.test(daring_phen_long$GDD, daring_phen_long$GDD_lag)
-
 plot(daring_phen_long$value~daring_phen_long$value_lag)
 cor.test(daring_phen_long$value, daring_phen_long$value_lag)
+
 #logged 
 plot(log(daring_phen_long$value+1)~(daring_phen_long$value_lag+1))
 cor.test(log(daring_phen_long$value+1),log(daring_phen_long$value_lag+1))
@@ -45,10 +29,10 @@ cor.test(log(daring_phen_long$value+1),log(daring_phen_long$value_lag+1))
 #final cleaning----
 #load plot numbers
 #only for spp with OTCs!!
-plots<-read.csv("data/Daring_raw_data/plantidmaps.csv")%>%rename(plantid=plant_id, treatment=otc_treatment)
-daring_phen_long<-left_join(daring_phen_long, plots)
-daring_phen_long$plot<-as.character(as.numeric(daring_phen_long$plot))
-names(daring_phen_long)
+#plots<-read.csv("data/Daring_raw_data/plantidmaps.csv")%>%rename(plantid=plant_id, treatment=otc_treatment)
+#daring_phen_long<-left_join(daring_phen_long, plots)
+#daring_phen_long$plot<-as.character(as.numeric(daring_phen_long$plot))
+#names(daring_phen_long)
 
 #take off numbers after traits
 daring_phen_long$trait<-substring(daring_phen_long$trait,1,nchar(daring_phen_long$trait)-2)
@@ -95,19 +79,7 @@ daring_phen_long<-mutate(daring_phen_long,
                          phen2=if_else(is.na(phen2), phen, phen2))
 
 
-#plot
-specColor <- c(
-  "#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7",
-  "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD",
-  "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D",
-  "#8A7C64", "#599861", "#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7",
-  "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD",
-  "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D",
-  "#8A7C64", "#599861")
-
-
 #reclassifying to num flowers, num fruits, repro_size equivalents
-##SLOW!! 
 daring_phen_long<-mutate(daring_phen_long, trait2=case_when(
   trait=="num_flowers"|trait=="num_flowers_per_stalk"|trait=="num_catkins"|trait=="num_flowering_stalks"|
     trait=="num_female_catkins"~"num_flowers", 
@@ -122,19 +94,12 @@ daring_phen_long<-mutate(daring_phen_long,
 flower_open<-subset(daring_phen_long, phen2=="first_flower_open")
 flower_open<-subset(flower_open, trait2=="num_flowers"|trait2=="num_fruit"|trait2=="repro_size"|
                       trait2=="diameter_mm"|trait=="growth_inc_mm"|trait=="leaf_length_mm")
-flower_bud<-subset(daring_phen_long, phen2=="first_flower_bud")
-flower_bud<-subset(flower_bud, trait2=="num_flowers"|trait2=="num_fruit"|trait2=="repro_size"|
-                     trait2=="diameter_mm"|trait=="growth_inc_mm"|trait=="leaf_length_mm")
+#flower_bud<-subset(daring_phen_long, phen2=="first_flower_bud")
+#flower_bud<-subset(flower_bud, trait2=="num_flowers"|trait2=="num_fruit"|trait2=="repro_size"|
+#                     trait2=="diameter_mm"|trait=="growth_inc_mm"|trait=="leaf_length_mm")
 
-specColor <- c(
-  "#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7",
-  "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD",
-  "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D",
-  "#8A7C64", "#599861", "#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7",
-  "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD",
-  "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D",
-  "#8A7C64", "#599861")
-
+#plot
+source('scripts/colorscale.R')
 ggplot(flower_open,
        aes(x=scale(doy), y=log(value+1), fill=species))+
   geom_point(aes(colour=species), alpha=0.5)+
@@ -145,19 +110,8 @@ ggplot(flower_open,
   theme_bw()+  facet_wrap(~trait2+phen2,scales = "free")+ 
   ylab("trait value (log)")+ xlab("DOY flower open")
 
-ggplot(flower_bud,
-       aes(x=scale(doy), y=log(value+1), fill=species))+
-  geom_point(aes(colour=species), alpha=0.5)+
-  geom_smooth(method = lm, formula = y ~ splines::bs(x, 3)) + #cubic spline
-  geom_smooth(method='lm') +
-  scale_fill_manual(values=specColor)+ scale_color_manual(values=specColor)+
-geom_smooth(method='gam', formula= y ~ s(x, bs = "cs", fx = TRUE, k = 2)) + 
-  theme_bw()+  facet_wrap(~trait2+phen2,scales = "free")+ 
-  ylab("trait value (log)")+ xlab("DOY flower open")
-
 
 #make sure only 1 measurement type per species
 check<-group_by(flower_open, species, trait2)%>%summarise(count=n_distinct(trait2))
-check2<-group_by(flower_bud, species, trait2)%>%summarise(count=n_distinct(trait2))
 
-save(daring_phen_long, flower_open, flower_bud, file="data/Mixed_mods_df.Rdata")
+save(daring_phen_long, flower_open, file="data/Mixed_mods_df.Rdata")
