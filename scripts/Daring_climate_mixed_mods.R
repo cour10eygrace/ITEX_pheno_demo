73cb5a2e8e762d75edd8a7c04eb256535eb9fd1d
cour10eygrace <courtneygrace125@gmail.com>
2023-03-02 13:27:30 -0800

    clean up script, add prob fruiting

--- scripts/Daring_climate_mixed_mods.R
+++ scripts/Daring_climate_mixed_mods.R
@@ -14,16 +14,116 @@ load(file="data/Mixed_mods_df.Rdata")
 
 #add in season lags 
 flower_open<-left_join(flower_open, seas_clim)
-flower_bud<-left_join(flower_bud, seas_clim)
+
 #deal with summer_lag for year 0 (2001) replace with current year Summer
 flower_open<-mutate(flower_open, Summer_lag=if_else(is.na(Summer_lag), Summer, Summer_lag))
 
 
-semdat<-select(flower_open, species, year, plantid, plot, treatment, phen, trait, value, doy, GDD, GDD_lag, sfDOY,  phen2, trait2, Spring, Fall, Summer, 
+semdat<-select(flower_open, species, year, plantid, treatment, phen, trait, value, doy, sfDOY,  phen2, trait2, Spring, Fall, Summer, 
                Spring_lag, Summer_lag, Fall_lag, Summer_diff) 
 semdat<-subset(semdat, treatment=="CTL") #only keep control data - look at OTCs separately 
 
-#look at general relationships- what do we expect?
+
+#lmers
+#flowering DOY
+doy1<-lmer(doy~ Summer+ (1|species:plantid) + (1|year) + (1|species), semdat)
+summary(doy1)
+doy1x<-lmer(doy~ Summer_lag+ (1|species:plantid) + (1|year) + (1|species), semdat)
+summary(doy1x)
+doy1y<-lmer(doy~ sfDOY+ (1|species:plantid) + (1|year) + (1|species), semdat)
+summary(doy1y)
+doy2<-lmer(doy~ Summer+ Summer_lag + (1|species:plantid) + (1|year) + (1|species), semdat)
+summary(doy2)
+doy2x<-lmer(doy~ Summer+ sfDOY + (1|species:plantid) + (1|year) + (1|species), semdat)
+summary(doy2x)
+doy2y<-lmer(doy~ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), semdat)
+summary(doy2y)
+doy3<-lmer(doy~ Summer+ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), semdat)
+summary(doy3)
+
+#model selection
+aictab(cand.set = c(doy1, doy1x, doy1y, doy2, doy2x, doy2y, doy3), 
+       modnames = c("doy1", "doy1x", "doy1y", "doy2", "doy2x", "doy2y", "doy3"))
+#Summer+ SF, Summer + Summer_lag + SF, Summer, Summer + Summer_lag all equal 
+#use most simple model summer only & for consistency with flower number (below)
+#use summer + summer lag in fruit models 
+
+#flower number 
+flowdat<-subset(semdat, trait2=="num_flowers"& value>0)
+hist(flowdat$value)
+hist(log(flowdat$value))
+
+flow1<-lmer(log(value)~ Summer+ (1|species:plantid) + (1|year) + (1|species), flowdat)
+summary(flow1)
+flow1x<-lmer(log(value)~ Summer_lag+ (1|species:plantid) + (1|year) + (1|species), flowdat)
+summary(flow1x)
+flow1y<-lmer(log(value)~ sfDOY+ (1|species:plantid) + (1|year) + (1|species), flowdat)
+summary(flow1y)
+flow2<-lmer(log(value)~ Summer+ Summer_lag + (1|species:plantid) + (1|year) + (1|species), flowdat)
+summary(flow2)
+flow2x<-lmer(log(value)~ Summer+ sfDOY + (1|species:plantid) + (1|year) + (1|species), flowdat)
+summary(flow2x)
+flow2y<-lmer(log(value)~ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), flowdat)
+summary(flow2y)
+flow3<-lmer(log(value)~ Summer+ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), flowdat)
+summary(flow3)
+
+aictab(cand.set = c(flow1, flow1x, flow1y, flow2, flow2x, flow2y, flow3), 
+       modnames = c("flow1", "flow1x", "flow1y","flow2", "flow2x", "flow2y", "flow3"))
+#Summer only best model delta aic >2 
+
+
+#prob fruit 
+fruitdat<-subset(semdat, trait2=="num_fruit")%>%mutate(probfruit=ifelse(value==0, 0, 1))
+hist(fruitdat$probfruit)
+
+pfruit1<-glmer(probfruit~ Summer+ (1|species:plantid) + (1|year) + (1|species), fruitdat, family = binomial())
+summary(pfruit1)
+pfruit1x<-glmer(probfruit~ Summer_lag+ (1|species:plantid) + (1|year) + (1|species), fruitdat,  family = binomial())
+summary(pfruit1x)
+pfruit1y<-glmer(probfruit~ sfDOY+ (1|species:plantid) + (1|year) + (1|species), fruitdat, family = binomial())
+summary(pfruit1y)
+pfruit2<-glmer(probfruit~ Summer+ Summer_lag + (1|species:plantid) + (1|year) + (1|species), fruitdat, family = binomial())
+summary(pfruit2)
+pfruit2x<-glmer(probfruit~ Summer+ sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat, family = binomial())
+summary(pfruit2x)
+pfruit2y<-glmer(probfruit~ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat, family = binomial())
+summary(pfruit2y)
+pfruit3<-glmer(probfruit~ Summer+ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat, family = binomial())
+summary(pfruit3)
+
+aictab(cand.set = c(pfruit1, pfruit1x, pfruit1y, pfruit2, pfruit2x, pfruit2y, pfruit3), 
+       modnames = c("pfruit1", "pfruit1x", "pfruit1y", "pfruit2", "pfruit2x", "pfruit2y", "pfruit3"))
+#Summer lag only is best model but lots of other models are equivalent. Use summer lag for consistency with fruit number (below)
+
+
+#fruit number 
+fruitdat<-subset(fruitdat, value>0)
+hist(fruitdat$value)
+hist(log(fruitdat$value))
+
+fruit1<-lmer(log(value)~ Summer+ (1|species:plantid) + (1|year) + (1|species), fruitdat)
+summary(fruit1)
+fruit1x<-lmer(log(value)~ Summer_lag+ (1|species:plantid) + (1|year) + (1|species), fruitdat)
+summary(fruit1x)
+fruit1y<-lmer(log(value)~ sfDOY+ (1|species:plantid) + (1|year) + (1|species), fruitdat)
+summary(fruit1y)
+fruit2<-lmer(log(value)~ Summer+ Summer_lag + (1|species:plantid) + (1|year) + (1|species), fruitdat)
+summary(fruit2)
+fruit2x<-lmer(log(value)~ Summer+ sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat)
+summary(fruit2x)
+fruit2y<-lmer(log(value)~ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat)
+summary(fruit2y)
+fruit3<-lmer(log(value)~ Summer+ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat)
+summary(fruit3)
+
+aictab(cand.set = c(fruit1, fruit1x, fruit1y, fruit2, fruit2x, fruit2y, fruit3), 
+       modnames = c("fruit1", "fruit1x", "fruit1y", "fruit2", "fruit2x", "fruit2y", "fruit3"))
+#Summer lag only best model delta aic >2 
+
+
+
+##look at general relationships- what do we expect?
 #change in temps over time 
 ggplot(semdat,
        aes(x=as.numeric(year), y=Summer))+
@@ -32,7 +132,7 @@ ggplot(semdat,
   xlab("Year")+ ylab("Growing Season temp (C)")
 
 summary(lm(Summer~as.numeric(year), semdat))
-#0.116*20
+#0.116*21= 2.4 C
 
 #temporal trend in snow free much weaker but also higher variation (by sites)
 ggplot(semdat,
@@ -144,75 +244,3 @@ cor.test(fruitdat$value, fruitdat$Spring_lag)#0.17
 hist(fruitdat$doy)
 hist(log(fruitdat$value))
 
-
-#lmers
-#flowering DOY
-doy1<-lmer(doy~ Summer+ (1|species:plantid) + (1|year) + (1|species), semdat)
-summary(doy1)
-doy1x<-lmer(doy~ Summer_lag+ (1|species:plantid) + (1|year) + (1|species), semdat)
-summary(doy1x)
-doy1y<-lmer(doy~ sfDOY+ (1|species:plantid) + (1|year) + (1|species), semdat)
-summary(doy1y)
-doy2<-lmer(doy~ Summer+ Summer_lag + (1|species:plantid) + (1|year) + (1|species), semdat)
-summary(doy2)
-doy2x<-lmer(doy~ Summer+ sfDOY + (1|species:plantid) + (1|year) + (1|species), semdat)
-summary(doy2x)
-doy2y<-lmer(doy~ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), semdat)
-summary(doy2y)
-doy3<-lmer(doy~ Summer+ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), semdat)
-summary(doy3)
-
-#model selection
-aictab(cand.set = c(doy1, doy1x, doy1y, doy2, doy2x, doy2y, doy3), 
-       modnames = c("doy1", "doy1x", "doy1y", "doy2", "doy2x", "doy2y", "doy3"))
-#Summer, Summer + Summer_lag, Summer+ SF, Summer + Summer_lag + SF all equal 
-#use most simple model =summer only
-
-#flower number 
-flowdat<-subset(semdat, trait2=="num_flowers"& value>0)
-hist(flowdat$value)
-hist(log(flowdat$value))
-
-flow1<-lmer(log(value)~ Summer+ (1|species:plantid) + (1|year) + (1|species), flowdat)
-summary(flow1)
-flow1x<-lmer(log(value)~ Summer_lag+ (1|species:plantid) + (1|year) + (1|species), flowdat)
-summary(flow1x)
-flow1y<-lmer(log(value)~ sfDOY+ (1|species:plantid) + (1|year) + (1|species), flowdat)
-summary(flow1y)
-flow2<-lmer(log(value)~ Summer+ Summer_lag + (1|species:plantid) + (1|year) + (1|species), flowdat)
-summary(flow2)
-flow2x<-lmer(log(value)~ Summer+ sfDOY + (1|species:plantid) + (1|year) + (1|species), flowdat)
-summary(flow2x)
-flow2y<-lmer(log(value)~ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), flowdat)
-summary(flow2y)
-flow3<-lmer(log(value)~ Summer+ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), flowdat)
-summary(flow3)
-
-aictab(cand.set = c(flow1, flow1x, flow1y, flow2, flow2x, flow2y, flow3), 
-       modnames = c("flow1", "flow1x", "flow1y","flow2", "flow2x", "flow2y", "flow3"))
-#Summer only best model delta aic >2 
-
-
-#fruit number 
-fruitdat<-subset(semdat, trait2=="num_fruit")
-hist(fruitdat$value)
-hist(log(fruitdat$value+1))
-
-fruit1<-lmer(log(value+1)~ Summer+ (1|species:plantid) + (1|year) + (1|species), fruitdat)
-summary(fruit1)
-fruit1x<-lmer(log(value+1)~ Summer_lag+ (1|species:plantid) + (1|year) + (1|species), fruitdat)
-summary(fruit1x)
-fruit1y<-lmer(log(value+1)~ sfDOY+ (1|species:plantid) + (1|year) + (1|species), fruitdat)
-summary(fruit1y)
-fruit2<-lmer(log(value+1)~ Summer+ Summer_lag + (1|species:plantid) + (1|year) + (1|species), fruitdat)
-summary(fruit2)
-fruit2x<-lmer(log(value+1)~ Summer+ sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat)
-summary(fruit2x)
-fruit2y<-lmer(log(value+1)~ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat)
-summary(fruit2y)
-fruit3<-lmer(log(value+1)~ Summer+ Summer_lag +  sfDOY + (1|species:plantid) + (1|year) + (1|species), fruitdat)
-summary(fruit3)
-
-aictab(cand.set = c(fruit1, fruit1x, fruit1y, fruit2, fruit2x, fruit2y, fruit3), 
-       modnames = c("fruit1", "fruit1x", "fruit1y", "fruit2", "fruit2x", "fruit2y", "fruit3"))
-#Summer lag only best model delta aic >2 
