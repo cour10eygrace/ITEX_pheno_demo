e547bae54242fe48fa60d90420a6be413439f141
cour10eygrace <courtneygrace125@gmail.com>
2023-03-06 15:43:11 -0800

    comment out prior visits code

--- scripts/Alex_phen_QAQC.R
+++ scripts/Alex_phen_QAQC.R
@@ -157,85 +157,75 @@ save(alex_phen, file='data/alex_cleaned_phen.Rdata')
 # -- ASSIGN PRIOR VISITS ----
 ##NOT USING CURRENTLY bc no dates with ranges listed 
 #pull out all observation dates for each year x site
-obsdates <- alex_phen_long %>% select(year, site, doy) %>% distinct()
+#obsdates <- alex_phen_long %>% select(year, site, doy) %>% distinct()
 # reassign names
-names(obsdates) <- c('year', 'site', "Obs.date")
+#names(obsdates) <- c('year', 'site', "Obs.date")
 
 #find observation date with the minimum difference to the doy of the phenstage for the same site x year
 #often the obs snow free
-phenxx<-left_join(alex_phen_long, obsdates)%>%group_by(year, site, otc_treatment)%>%filter(!is.na(doy))%>%
-  mutate(diff=doy-Obs.date)%>%group_by(year, species,site, otc_treatment,doy, phen)%>%filter(diff>0)%>%
-  slice(which.min(diff))%>%
-  select(year, species, site, otc_treatment, phen, Obs.date, diff)
+#phenxx<-left_join(alex_phen_long, obsdates)%>%group_by(year, site, otc_treatment)%>%filter(!is.na(doy))%>%
+ # mutate(diff=doy-Obs.date)%>%group_by(year, species,site, otc_treatment,doy, phen)%>%filter(diff>0)%>%
+#  slice(which.min(diff))%>%
+#  select(year, species, site, otc_treatment, phen, Obs.date, diff)
 
-hist(phenxx$diff)
+#hist(phenxx$diff)
 
 #join with full dataset
 #prior visit=min obs date
-alex_phen_long<-left_join(alex_phen_long, phenxx)%>%
-  rename(prior_visit= Obs.date)
+#alex_phen_long<-left_join(alex_phen_long, phenxx)%>%
+ # rename(prior_visit= Obs.date)
 
 #check all phenology where pv not assigned
-pvmiss<-subset(alex_phen_long, is.na(prior_visit))
+#pvmiss<-subset(alex_phen_long, is.na(prior_visit))
 #use snow free for this
-sf<-read.csv("data/snowfree_data.csv")
-sf<-subset(sf, site_name=="ALEXFIORD")%>%select(-site_name)%>%rename(sf_doy=doy)
+#sf<-read.csv("data/Alex_raw_data/snowfree_data.csv")
+#sf<-subset(sf, site_name=="ALEXFIORD")%>%select(-site_name)%>%rename(sf_doy=doy)
 
 #join with full dataset
-alex_phen_long<-left_join(alex_phen_long, sf)%>%
-  mutate(prior_visit=if_else(is.na(prior_visit), sf_doy, prior_visit))
+#alex_phen_long<-left_join(alex_phen_long, sf)%>%
+ # mutate(prior_visit=if_else(is.na(prior_visit), sf_doy, prior_visit))
 #reset as numeric
-alex_phen_long$prior_visit<-as.numeric(alex_phen_long$prior_visit)
+#alex_phen_long$prior_visit<-as.numeric(alex_phen_long$prior_visit)
 
 #look at prior visits 
 #find earliest/latest observed dates across all years for each spp x phenophase
-pv_mins<-group_by(alex_phen_long, species,site, phen)%>%
-  summarise(pv_min=min(prior_visit, na.rm = T), doy_max=max(doy, na.rm = T))
+#pv_mins<-group_by(alex_phen_long, species,site, phen)%>%
+#  summarise(pv_min=min(prior_visit, na.rm = T), doy_max=max(doy, na.rm = T))
 
 #join with full dataset 
 # anything where the prior visit is not assigned 
 #replace with min prior visit for that spp x phenophase from across all years
 
-alex_phen_long<-left_join(alex_phen_long, pv_mins)%>%
-  mutate(prior_visit=if_else(is.na(prior_visit), pv_min, prior_visit))
+#alex_phen_long<-left_join(alex_phen_long, pv_mins)%>%
+#  mutate(prior_visit=if_else(is.na(prior_visit), pv_min, prior_visit))
 
 #check that all prior visits are earlier than doys & vice-versa- remove if not
-alex_phen_long<-alex_phen_long%>% mutate(check=doy-pv_min, check2=doy_max-prior_visit)%>%
-  filter(check>=0)
+#alex_phen_long<-alex_phen_long%>% mutate(check=doy-pv_min, check2=doy_max-prior_visit)%>%
+#  filter(check>=0)
 
 #then take average bw prior visit and doy as 'censored' phen observation
-alex_phen_long$DOY<-round((alex_phen_long$doy+alex_phen_long$prior_visit)/2)
-plot(alex_phen_long$DOY~alex_phen_long$doy)#visualize - error gets worse for later phenophases 
+##alex_phen_long$DOY<-round((alex_phen_long$doy+alex_phen_long$prior_visit)/2)
+#plot(alex_phen_long$DOY~alex_phen_long$doy)#visualize - error gets worse for later phenophases 
 
 
-ggplot(alex_phen_long, aes(DOY, fill=species))+
-  geom_histogram(alpha=0.7)+ scale_fill_manual(values=specColor[c(1:5,34:36,38:40)]) +
-  facet_wrap(~phen+otc_treatment, scales="free")+ theme_bw()
+#ggplot(alex_phen_long, aes(DOY, fill=species))+
+#  geom_histogram(alpha=0.7)+ scale_fill_manual(values=specColor[c(1:5,34:36,38:40)]) +
+#  facet_wrap(~phen+otc_treatment, scales="free")+ theme_bw()
 
 #pivot wider
-alex_phen_long<-select(alex_phen_long, -doy, -diff, -check, -prior_visit, -sf_doy, -check2, 
-                       -pv_min, -doy_max)
+#alex_phen_long<-select(alex_phen_long, -doy, -diff, -check, -prior_visit, -sf_doy, -check2, 
+#                       -pv_min, -doy_max)
 
-alex_phen2<-pivot_wider(alex_phen_long, names_from = "phen", values_from = "DOY", 
-                        values_fn=mean) #66 obs not uniquely identified - take average
+#alex_phen2<-pivot_wider(alex_phen_long, names_from = "phen", values_from = "DOY", 
+ #                       values_fn=mean) #66 obs not uniquely identified - take average
 
 #combine back with traits 
-alex_traits<-pivot_wider(alex_trait_long, names_from = "trait", values_from = "value", 
-                         values_fn=mean) 
+#alex_traits<-pivot_wider(alex_trait_long, names_from = "trait", values_from = "value", 
+        #                 values_fn=mean) 
 #181 obs not uniquely identified - mostly Luzula , take average 
-alex_phen<-left_join(alex_phen2, alex_traits)
+#alex_phen<-left_join(alex_phen2, alex_traits)
 
-save(alex_phen, file='data/alex_cleaned_phen_censored.Rdata')
+#save(alex_phen, file='data/alex_cleaned_phen_censored.Rdata')
 
 
-
-####create DOY standardized off snow free dates----
-#for comparison with DL
-#read in data available
-sfx<-read.csv("data/snowfree_data.csv")
-alexsf<-subset(sfx, site_name=="ALEXFIORD")%>%
-  separate(., subsite, into = c("x", "site", "y"))%>%select(-x,-y, -OTCWinterRemoval, -OTC_period)
-#read in additional years (raw)
-sfy<-read.csv("data/Alex_raw_data/snow/compiled_claude_snow_depth.csv")%>%
-  select(.,year, Snow_melt_day)%>%distinct(.)%>%filter(year>2012)
 
